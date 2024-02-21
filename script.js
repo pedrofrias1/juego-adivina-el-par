d1d8ecbfdcfb41c9daa97846f5458ecac4c09532
pedro frias <pepof610@gmail.com>
2024-02-21 20:38:33 -0300

    lograr repartir 3 cartas con for

--- script.js
+++ script.js
@@ -2,13 +2,17 @@
 let cartas = document.querySelector(".cartas");
 let paloArriba = document.querySelector(".paloArriba");
 let numeros = document.querySelector(".numeros");
+
+
+
 // let paloAbajo = document.querySelector(".paloAbajo")
 
 
 // valores y cartas .
 let palo = ["espada","basto","oro","copa"]
 let numero = ["1" , "2" , "3" , "4" , "5" , "6" , "7" , "10" , "11" , "12" ];
-const totalCartas= 40;
+const totalCards=3;
+let cards=[];
 
 
 // let palos={
@@ -18,22 +22,47 @@ const totalCartas= 40;
 //     oro:Math.floor(Math.random()*numeros.length)+" de oro",
 // }
 
+//generar la carta.
 const crearCarta=()=>{
 
     let valorSimple=palo[Math.floor(Math.random()*palo.length)];
     let simpleNumero=numero[Math.floor(Math.random()*numero.length)];
     let enseñar= `${simpleNumero}  ${valorSimple}`;
+    let card=cartas.innerHTML= `<h4>${enseñar}</h4>`;
+
     
-    console.dir(valorSimple);
     
-     for (let i = 0; i <= 3; i++) {
+     for (let i = 0; i < totalCards; i++) {
+        let carta = card;
+        cards.push(carta)
+        let nuevaCartas = document.createElement("div");
+        nuevaCartas.classList.add("cartas");
+        
+        nuevaCartas.innerHTML=`<h4>${enseñar}</h4>`;
+        
+        
+
+
+        console.log(nuevaCartas);
+            
+           
+            
+            
+
             if (valorSimple==="espada"){
                 cartas.classList.add("espada")
             }
+            if (valorSimple==="oro") {
+                cartas.classList.add("oro");
+            }
+            if (valorSimple==="basto") {
+                cartas.classList.add("basto");
+            }
+            if (valorSimple==="copa") {
+                cartas.classList.add("copa");
+            }
 
-        let card=cartas.innerHTML= `<h4>${enseñar}</h4> 
-                                     
-        `
+        
         
 
     }
@@ -41,73 +70,12 @@ const crearCarta=()=>{
     
 
 
-    const mostrarCarta=()=>{
-      
-       
-        
-        
-    }
-    mostrarCarta()
+    
 
 
    
 }
 crearCarta();
 
-//const palos = ["corazon", "diamante", "pica", "trebol"];
-// const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-// let paloArriba = document.querySelector("#simboloArriba");
-// let numero = document.querySelector("#numero");
-// let paloAbajo = document.querySelector("#simboloAbajo");
-
-
-// function eleccionCarta(arr1, arr2){
-//     let carta =[]
-//     carta.push(arr1[Math.floor(Math.random()*arr1.length)]);
-//     carta.push(arr2[Math.floor(Math.random()*arr2.length)]);
-//     return carta
-// }
-// function asignarColor(arr){
-//     if(arr[0] === "corazon" || arr[0] === "diamante"){
-//         paloArriba.style.color = "red";
-//         numero.style.color = "red";
-//         paloAbajo.style.color = "red";
-//     }
-//     else{
-//         paloArriba.style.color = "black";
-//         numero.style.color = "black";
-//         paloAbajo.style.color = "black";
-//     }
-// }
-// function asignarPalo(arr){
-//     if(arr[0] === "corazon"){
-//         paloArriba.innerHTML = "♥"
-//         paloAbajo.innerHTML = "♥"
-//     }
-//     else if(arr[0] === "diamante"){
-//         paloArriba.innerHTML = "♦"
-//         paloAbajo.innerHTML = "♦"
-//     }
-//     else if(arr[0] === "trebol"){
-//         paloArriba.innerHTML = "♣"
-//         paloAbajo.innerHTML = "♣"
-//     }
-//     else{
-//         paloArriba.innerHTML = "♠"
-//         paloAbajo.innerHTML = "♠"
-//     }
-// }
-// function asignarValor(arr){
-//     numero.innerHTML = arr[1];
-// }
-// function generacionCarta(){
-//     let carta = eleccionCarta(palos, valores);
-//     asignarColor(carta);
-//     asignarPalo(carta);
-//     asignarValor(carta);
-// }
-
-// window.onload = function(){generacionCarta()}
-// document.querySelector(".boton").addEventListener("click", generacionCarta)
 
 
